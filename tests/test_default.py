7fed2002450a2375fb50d6b4625f6b49b818f1a6
Sean McGowan <(none)>
2018-07-28 16:07:32 -0400

    Fix verbosity for tests and update tests

--- tests/test_default.py
+++ tests/test_default.py
@@ -4,9 +4,22 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     '.molecule/ansible_inventory').get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_ntp_is_installed(host):
+    p = host.package("ntp")
+    assert p.is_installed
+
+
+def test_ntp_conf(host):
+    f = host.file('/etc/ntp.conf')
 
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+    assert f.mode == 0o644
+
+
+def test_ntp_is_installed_and_enabled(host):
+    s = host.service('ntpd')
+
+    assert s.is_running
+    assert s.is_enabled
